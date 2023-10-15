d6b197549c621d2a5d86400cf3dbe0cfd1c0c0b3
Jahid Hasan <jahid.hasan.shanto@gmail.com>
2023-10-15 06:40:48 +0600

    partials of page, devided by catagory

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+
+$heading='About Page';
+
+$bgAbout ='bg-gray-900 text-white';
+
+require "views/about.view.php";
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+$heading ='Home Page';
+
+$bgHome ='bg-gray-900 text-white';
+
+require "views/index.view.php";
